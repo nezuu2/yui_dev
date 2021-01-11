1912649cdf83b602b87ed50d1a8e583e854d824e
nezuu2 <nezuu_pc2@yahoo.co.jp>
2021-01-12 08:24:54 +0900

    チャット送信可能となる

--- chat.js
+++ chat.js
@@ -2,6 +2,7 @@ $(function(){
 	$("#chat-button").click(function() {
 		// 値を取得
         const str1 = $('input:text[id="chat-input"]').val();
-        alert('送ったメッセージ：\n' + str1);
-	});
+        $('#chat2').text(str1);
+        $('input:text[id="chat-input"]').val(""); //クリア
+    });
 });
