370c5fe4b2001b1120d909a6a3a5cc491929ca0d
nezuu2 <nezuu_pc2@yahoo.co.jp>
2021-01-12 07:38:37 +0900

    文字を所得できるようにしました

--- chat.js
+++ chat.js
@@ -1,5 +1,7 @@
 $(function(){
-    $('#chat-button').on('click', function() {
-    alert("クリックされました");
-    });
+	$("#chat-button").click(function() {
+		// 値を取得
+        const str1 = $('input:text[id="chat-input"]').val();
+        alert('送ったメッセージ：\n' + str1);
+	});
 });
