a81d1dab34585d5e6c1ff5e783f131730f627a3d
nezuu2 <nezuu_pc2@yahoo.co.jp>
2021-01-12 09:57:52 +0900

    チャット機能の実装(仮)

--- chat.js
+++ chat.js
@@ -4,5 +4,11 @@ $(function(){
         const str1 = $('input:text[id="chat-input"]').val();
         $('#chat2').text(str1);
         $('input:text[id="chat-input"]').val(""); //クリア
+
+        if (str1 == 'こんにちは' || str1 == 'こんばんは' || str1 == 'おはよう') {
+            $('#chat3').text('😊');
+        } else {
+            $('#chat3').text('');
+        }
     });
 });
