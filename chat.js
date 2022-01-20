e3bf83b22c6abf9c8c17a135118ec2a345b9c5ab
nezuu2 <nezuu_pc2@yahoo.co.jp>
2022-01-20 09:42:29 +0900

    機能の追加と訂正

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('#ver').text('version beta0.1');
+    $('#ver').text('version beta0.2');
 
 	$("#chat-button").click(function() {
 		// 値を取得
@@ -11,22 +11,45 @@ $(function(){
         } else if (str == 'Before') {
             $('#chat').prepend('<ul id="chat add" class="chat">After</ul>');
         } else if (str == 'test') {
-            $('#chat').prepend('<ul id="chat add" class="chat">test</ul>');
+            $('#chat').prepend('<p id="chat add" class="chat">test</p>');
         } else if (str == 'YUIちゃん') {
             $('#chat').prepend('<ul id="chat add" class="chat">はい どういたしましたか？</ul>');
         } else if (str == 'お話できる？') {
             $('#chat').prepend('<ul id="chat add" class="chat">ごめんなさい。体験版だと製品版よりあまりうまくお話できないの…<br>ごめんね💦。</ul>');
         } else if (str == 'アプリをダウンロードしたい') {
-            $('#chat').prepend('<ul id="chat add" class="chat">承知しました!今からURLを送りますね！</ul>');
+            $('#chat').prepend('<ul id="chat add" class="chat">ありがとうございます!!<br>こちらからぜひダウンロードしてください</ul><a href="https://play.google.com/store/apps/details?id=com.websarva.wings.android.yui" class="btn btn--black">アプリをダウンロード</a>');
         } else if (str == '気に入った') {
-            $('#chat').prepend('<ul id="chat add" class="chat">ありがとうございます!。よければ製品版でもお話してくださいね★。</ul>');
+            $('#chat').prepend('<ul id="chat add" class="chat">ありがとうございます!<br>よければ製品版でもお話してくださいね★。</ul><a href="https://play.google.com/store/apps/details?id=com.websarva.wings.android.yui" class="btn btn--black">アプリをダウンロード</a>');
         } else if (str == 'Can you english typing?') {
             $('#chat').prepend('<ul id="chat add" class="chat">No, sorry.<br>I can only this engish typing.</ul>');
-        } else if (str == 'Can you tooking to me?') {
-            $('#chat').prepend('<ul id="chat add" class="chat">No, sorry.<br>Becouse is i cant understand it will english.</ul>');
+        } else if (str == 'Can you talk to me?') {
+            $('#chat').prepend('<ul id="chat add" class="chat">No, sorry.<br>Becouse is I cant understand it will english.</ul>');
         }
          else {
             $('#chat').prepend('<ul id="chat add" class="chat">？</ul>');
         }
+    });
+
+    $("#mic-button").click(function() {
+		var rec = new webkitSpeechRecognition();
+        rec.continuous = false; // 後述
+        rec.interimResults = true; // trueにすると認識途中の結果も返す
+        rec.lang = 'ja-JP'; // 言語を指定する
+        rec.maxAlternatives = 1; // 結果候補の最大数(デフォルトは1)
+        rec.onresult = e => {
+        // 認識結果がSpeechRecognitionEventインスタンスとして渡されます
+        // 認識した言葉を表示
+        $('input:text[id="chat-input"]').prepend(e.results[0][0].transcript);
+        rec.stop() // 認識が完了したら終了する
+        }
+        rec.start() // 認識を開始します
+    });
+
+    $(function speech(text) {
+        var u = new SpeechSynthesisUtterance();
+        u.text = document.querySelector('.input-text').value; // 発話する内容を指定する
+        u.lang = 'ja-JP'; // 言語を指定する
+        u.rate = 1.0; // 発話速度を指定する
+        speechSynthesis.speak(u); // 喋らせる
     });
 });
