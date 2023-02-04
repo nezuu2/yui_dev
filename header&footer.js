5680f9a378f834b3dd11ca5e50cad271bf4aa205
nezuu2 <nezuu_pc2@yahoo.co.jp>
2023-02-04 20:31:00 +0900

    header&footerを纏めてJSで追記可能にした

---
+++ header&footer.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('#header').append('<li><a href="project-member.html" class="menu tabs-button">YUIについて</a></li>',
+            '<li><a href="#" class="menu tabs-button">創始者もりけん。</a></li>',
+            '<li><a href="history.html" class="menu tabs-button">歴史</a></li>',
+            '<li><a href="news.html" class="menu tabs-button">NEWS</a></li>',
+            '<li><a href="contact-us.html" class="menu tabs-button">育成相談窓口</a></li>');
+
+{/* <li><a href="https://play.google.com/store/apps/details?id=com.websarva.wings.android.yui" class="menu tabs-button">アプリ</a></li>
+    <li><a href="counseling.html" class="menu tabs-button">カウンセリング</a></li>
+    <li><a href="javascript:alert('YouTubeアカウントは製作予定です。');" class="menu tabs-button black">YouTube</a></li>
+    <li><a href="https://twitter.com/Project_YUI816" class="menu tabs-button" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+    <li><a href="https://twitter.com/kuromoriken" class="menu tabs-button" target="_blank" rel="noopener noreferrer">開発者のTwitter</a></li> */}
+    
+    $('#footer').append('<li><a href="project-member.html" class="menu tabs-button">YUIについて</a></li>',
+            '<li><a href="#" class="menu tabs-button">創始者もりけん。</a></li>',
+            '<li><a href="history.html" class="menu tabs-button">歴史</a></li>',
+            '<li><a href="news.html" class="menu tabs-button">NEWS</a></li>',
+            '<li><a href="contact-us.html" class="menu tabs-button">育成相談窓口</a></li>');
+
+{/* <li><a href="will.html" class="button">志</a></li>
+    <li><a href="catch-copy.html" class="button">キャッチコピー</a></li>
+    <li><a href="credo.html" class="button">クレド</a></li>
+    <li><a href="project-member.html" class="button">Projectメンバー</a></li>
+    <li><a href="history.html" class="button">歴史</a></li>
+    <li><a href="contact-us.html" class="button">育成相談窓口</a></li>
+    <li><a href="materials-used.html" class="button">使用した素材</a></li>
+    <li><a href="privacy-policy.html" class="button">PrivacyPolicy</a></li> */}
+
+});
